2737733b2b3ab86727618b995bafd5483533db48
Diego Suarez <diegosuarezlopezalfonso@gmail.com>
2023-03-24 13:43:34 +0000

    trail

--- HELIXJUMP/Assets/Scripts/Pelota.cs
+++ HELIXJUMP/Assets/Scripts/Pelota.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Pelota : MonoBehaviour
 {
+    [SerializeField]
     Rigidbody PELOTArigidbody;
+
+    [SerializeField]
+    public float jumpForce = 2f;
+
+    [SerializeField]
+    GameObject trail;
 
     void awake()
     {
 
         PELOTArigidbody.GetComponent<Rigidbody>();
+        trail.SetActive(false);
 
     }
 
@@ -17,12 +25,16 @@ public class Pelota : MonoBehaviour
 
     void Update()
     {
-      //  Debug.Log(PELOTArigidbody.velocity.y);
+        //  Debug.Log(PELOTArigidbody.velocity.y);
+        trail.SetActive(PELOTArigidbody.velocity.y < -10.0f);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Hachocado");
+        PELOTArigidbody.velocity = Vector3.zero;
+        PELOTArigidbody.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+
     }
 
 
--- HELIXJUMP/Assets/Scripts/Rotacion.cs
+++ HELIXJUMP/Assets/Scripts/Rotacion.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotacion : MonoBehaviour
+/*public class Rotacion : MonoBehaviour
 {
     [SerializeField]
     public float speed;
     [SerializeField]
     private Vector3 rotation;
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(rotation * speed * Time.deltaTime);
+       
+        float Horizontal.getaxis,(Rigidbody.);
+
+       // float Vertical.getaxis,(Rigidbody.);
+
+
+        /*if (Input.GetKeyDown(KeyCode.d))
+        {
+            transform.Rotate(rotation * speed * Time.deltaTime);
+           
+        }
+
+        if (Input.GetKeyDown(KeyCode.e))
+        {
+            transform.Rotate(rotation * speed * Time.deltaTime);
+
+        }
     }
-}
+    
+} */
+
+
+
+
