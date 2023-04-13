bf1e3a37adef1c34096e2df6b1e4bb967e490d71
DielopAlf <114227973+DielopAlf@users.noreply.github.com>
2023-04-13 20:41:54 +0100

    menu

--- HELIXJUMP/Assets/Scripts/InterfazController.cs
+++ HELIXJUMP/Assets/Scripts/InterfazController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+using MyNamespace;
 
 public class InterfazController : MonoBehaviour
 {
     public TextMeshProUGUI puntospelota;
-   // public TextMeshProUGUI metrospelota;
+    public TextMeshProUGUI metrospelota;
     public TextMeshProUGUI textopuntos;
     public TextMeshProUGUI textmetros;
     public GameObject panelVictoria;
@@ -17,7 +18,6 @@ public class InterfazController : MonoBehaviour
     public Slider distanciaslider;
     public TextMeshProUGUI metrosvictoria;
     public TextMeshProUGUI metrosrecord;
-
 
     private void Awake()
     {
@@ -34,56 +34,64 @@ public class InterfazController : MonoBehaviour
     public void UpdatePuntos(int puntos)
     {
         textopuntos.text = "Puntos: " + puntos;
-
     }
-    public void updatepuntospelota(float puntos)
 
+    public void updatepuntospelota(float puntos)
     {
-        if (puntos>=3)
+        if (puntos >= 3)
         {
             puntospelota.gameObject.SetActive(true);
-            puntospelota.text = "+" +puntos;
+            puntospelota.text = "+" + puntos;
         }
         else
         {
-
             puntospelota.gameObject.SetActive(false);
-           
-
         }
     }
-   public void Updatetextmetros(int metros)
-     {
-         textmetros.text = "Metros: " + metros;
 
-     }
+    public void Updatetextmetros(int metros)
+    {
+        textmetros.text = "Metros: " + metros;
+    }
 
-    /* public void updatemetrospelota(float metros)
-
-
-   {
-       if (metros >= 1)
-       {
-           metrospelota.gameObject.SetActive(true);
-           metrospelota.text = "+" + metros;
-       }
-       else
-       {
-
-           metrospelota.gameObject.SetActive(false);
-
-
-       }
-   }*/
+     public void updatemetrospelota(float metros)
+    {
+        if (metros >= 1)
+        {
+            metrospelota.gameObject.SetActive(true);
+            metrospelota.text = "+" + metros;
+        }
+        else
+        {
+            metrospelota.gameObject.SetActive(false);
+        }
+    }
 
     public void MostrarPanelVictoria()
     {
+        int nivelActual = int.Parse(SceneManager.GetActiveScene().name.Replace("Nivel", ""));
+        int nivelAnterior = nivelActual - 1;
+
+        if (nivelAnterior >= 1)
+        {
+            string nombreNivelAnterior = "Nivel" + nivelAnterior;
+            if (!PlayerPrefs.HasKey(nombreNivelAnterior + "Completado") || !PlayerPrefs.GetInt(nombreNivelAnterior + "Completado").Equals(1))
+            {
+                Debug.Log("Completa el nivel anterior primero.");
+                return;
+            }
+        }
+
         panelVictoria.SetActive(true);
         Time.timeScale = 0f;
         textmetros.gameObject.SetActive(false);
         textopuntos.gameObject.SetActive(false);
-        metrosvictoria.text="Has recorrido "+ SistemadePuntos.instance.metros;
-        metrosrecord.text="El record es "+ PlayerPrefs.GetInt((SceneManager.GetActiveScene().name+"metros"));
+        metrosvictoria.text = "Has recorrido " + SistemadePuntos.instance.metros;
+        metrosrecord.text = "El record es " + PlayerPrefs.GetInt((SceneManager.GetActiveScene().name + "metros"));
+
+        // Marcar el nivel actual como completado en PlayerPrefs
+        PlayerPrefs.SetInt(SceneManager.GetActiveScene().name + "Completado", 1);
+        PlayerPrefs.Save();
     }
 
     public void MostrarPanelGameOver()
@@ -97,14 +105,14 @@ public class InterfazController : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
     public void ACTUALIZARDISTANCIA(float distancia)
     {
-        distanciaslider.value= distancia;
-
+        distanciaslider.value = distancia;
     }
+
     public void SalirJuego()
     {
         Application.Quit();
     }
-  
 }
--- HELIXJUMP/Assets/Scripts/MenuControl.cs
+++ HELIXJUMP/Assets/Scripts/MenuControl.cs
@@ -2,37 +2,130 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MenuControl : MonoBehaviour
-{ 
- public GameObject menuInicial;
- public GameObject menuNiveles;
 
-   public void cerrarjuego()
-   {
-    Application.Quit();
-   }
-   void Start()
-   {
-    menuInicial.SetActive(true);
-    menuNiveles.SetActive(false);
-   
-   }
-   public void botonplay()
-   {
-       menuInicial.SetActive(false);
-       menuNiveles.SetActive(true);
-       Debug.Log("jugar");
-   }
-    public void volver()
-   {
-       menuInicial.SetActive(true);
-       menuNiveles.SetActive(false);
+namespace MyNamespace
+{
+    public static class GameManager
+    {
+        public static bool nivel1Completado = false;
+        public static bool nivel2Completado = false;
+        public static bool nivel3Completado = false;
+        public static bool nivel4Completado = false;
+    }
 
-   }
-   public void cargarNivel(string nivel)
-   {
-        SceneManager.LoadScene(nivel);
+    public class MenuControl : MonoBehaviour
+    {
+        public GameObject menuInicial;
+        public GameObject menuNiveles;
 
+        void Start()
+        {
+            menuInicial.SetActive(true);
+            menuNiveles.SetActive(false);
+        }
 
-   }
+        public void cerrarjuego()
+        {
+            Application.Quit();
+        }
+
+        public void botonplay()
+        {
+            menuInicial.SetActive(false);
+            menuNiveles.SetActive(true);
+            Debug.Log("jugar");
+        }
+
+        public void volver()
+        {
+            menuInicial.SetActive(true);
+            menuNiveles.SetActive(false);
+        }
+
+        public void cargarNivel(string nivel)
+        {
+            switch (nivel)
+            {
+                case "nivel2":
+                    if (!GameManager.nivel1Completado)
+                    {
+                        Debug.Log("Completa el nivel anterior primero.");
+                        return;
+                    }
+                    break;
+                case "nivel3":
+                    if (!GameManager.nivel2Completado)
+                    {
+                        Debug.Log("Completa el nivel anterior primero.");
+                        return;
+                    }
+                    break;
+                case "nivel4":
+                    if (!GameManager.nivel3Completado)
+                    {
+                        Debug.Log("Completa el nivel anterior primero.");
+                        return;
+                    }
+                    break;
+                // Agrega casos para cada nivel que deba ser completado antes de acceder al siguiente
+            }
+
+            SceneManager.LoadScene(nivel);
+        }
+    }
 }
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MyNamespace
+{
+    public static class GameManager
+    {
+        // Usamos un diccionario para almacenar el estado completado de cada nivel
+        public static Dictionary<string, bool> nivelesCompletados = new Dictionary<string, bool>();
+    }
+
+    public class MenuControl : MonoBehaviour
+    {
+        public GameObject menuInicial;
+        public GameObject menuNiveles;
+
+        void Start()
+        {
+            menuInicial.SetActive(true);
+            menuNiveles.SetActive(false);
+        }
+
+        public void cerrarjuego()
+        {
+            Application.Quit();
+        }
+
+        public void botonplay()
+        {
+            menuInicial.SetActive(false);
+            menuNiveles.SetActive(true);
+            Debug.Log("jugar");
+        }
+
+        public void volver()
+        {
+            menuInicial.SetActive(true);
+            menuNiveles.SetActive(false);
+        }
+
+        public void cargarNivel(string nivel)
+        {
+            bool nivelCompletado;
+            if (GameManager.nivelesCompletados.TryGetValue(nivel, out nivelCompletado) && !nivelCompletado)
+            {
+                Debug.Log("Completa el nivel anterior primero.");
+                return;
+            }
+
+            SceneManager.LoadScene(nivel);
+        }
+    }
+}*/
