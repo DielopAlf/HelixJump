c9145d6b96a53cdaa2c90477962a77ad6ed746f8
Diego Suarez <diegosuarezlopezalfonso@gmail.com>
2023-04-14 12:17:01 +0100

    añadidos

---
+++ HELIXJUMP/Assets/Scripts/ButtonEnabled.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEnabled : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [SerializeField] int nivel = 0;
+    
+
+    // Update is called once per frame
+    void Awake()
+    {
+        int superado = PlayerPrefs.GetInt("NivelSuperado" + (nivel-1).ToString(), 0); 
+        GetComponent<Button>().interactable = superado == 1 ? true : false;
+    }
+}
--- HELIXJUMP/Assets/Scripts/Pelota.cs
+++ HELIXJUMP/Assets/Scripts/Pelota.cs
@@ -14,19 +14,27 @@ public class Pelota : MonoBehaviour
     private bool invencible = false;
     public float velocidadNormal;
     float velocidadprevia;
-   
+    // initialpotition
+    // enposition
+    //[]
+
     private void Awake()
     {
         pelotaRigidbody = GetComponent<Rigidbody>();
         trail.SetActive(false);
         velocidadNormal = ultraSpeed;
-        
+        //initialPoaition=tarb¡nsform.position.y;
+        //string
+        // debug
     }
 
     void Update()
     {
         trail.SetActive(pelotaRigidbody.velocity.y < -ultraSpeed);
-        velocidadprevia=pelotaRigidbody.velocity.magnitude;
+        velocidadprevia = pelotaRigidbody.velocity.magnitude;
+
+        //float metrosrecorridos=initialposition-transform.position.y;
+
         // Verifica si se alcanzó la velocidad de romper plataformas
         /*if (pelotaRigidbody.velocity.magnitude >= GetComponent<Plataformas>().velocidadParaRomperse)
         {
@@ -45,34 +53,34 @@ public class Pelota : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Plataforma"))
         {
-               
-                
+
+
 
             if (velocidadprevia >= velocidadMinimaParaRomper)
             {
                 collision.gameObject.SetActive(false);
             }
-             // Rebota en la plataforma
-                pelotaRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            // Rebota en la plataforma
+            pelotaRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
         else if (collision.gameObject.CompareTag("Plataformamala"))
         {
-             if (velocidadprevia >= velocidadMinimaParaRomper)
+            if (velocidadprevia >= velocidadMinimaParaRomper)
             {
                 collision.gameObject.SetActive(false);
                 pelotaRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 
             }
-            else 
+            else
             {
-             // Reinicia el juego si choca con una plataforma mala
+                // Reinicia el juego si choca con una plataforma mala
 
                 SistemadePuntos.instance.resetPuntos();
                 SistemadePuntos.instance.guardarmetrosprevios();
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
-           
-            
+
+
         }
         else if (collision.gameObject.CompareTag("META"))
         {
@@ -80,7 +88,10 @@ public class Pelota : MonoBehaviour
             SistemadePuntos.instance.gameOver();
             gameObject.SetActive(false);
             InterfazController.instance.MostrarPanelVictoria();
+            //guardardatos();
+
         }
+
     }
 
     IEnumerator TiempoInvencible()
@@ -101,4 +112,26 @@ public class Pelota : MonoBehaviour
         yield return new WaitForSeconds(5f);
         ultraSpeed = velocidadNormal;
     }
+    //stringlevel="metrosnivel"+nivel,tostring();
+    //if()playerprefs.getfloat(keylevel,0.0f)
+    //playerprefs.setfloat("metrosnivel" +nivel.tostring(),0,0f < metrosrecorridos);
+    //playerprefs.setint("nivelsuperado",nivel);
+    //player.save();
+
+    /* void guardardatos(bool haswon)
+   {
+     float metrosrecorridos = initialPosition - endPosition;
+     string keylevel = "MetrosNivel" + CambioNivel.ToString();
+     if (playerprefs.getfloat(keylevel,0.0f)< metrosrecorridos)
+     {
+     playerprefs.setfloat(keylevel,metrosrecorridos);
+     }
+     if(haswon == true)
+     {
+     playerprefs.setint("nivelsuperado" +nivel.tostring(),1);
+     }
+     playerprefs.save();
+
+   }*/
+    
 }
